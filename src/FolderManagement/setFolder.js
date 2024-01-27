7d832e36040dbf5efc86aa1e1efbff7e2875421d
Dzabol <sebastian.jablecki@gmail.com>
2024-01-27 23:52:24 +0100

    added folder functions

--- src/FolderManagement/setFolder.js
+++ src/FolderManagement/setFolder.js
@@ -1,11 +1,3 @@
-function testFolder() {
-    // let folder = setFolderToSetData_("1LTDyHFS4G-NXAauD1WwIUIfvD0yvI8CU", "2023")
-    // console.log(folder)
-
-    let = folderData = folderToSetData("1LTDyHFS4G-NXAauD1WwIUIfvD0yvI8CU", "2023")
-    console.log(folderData)
-}
-
 /**
  * *****************************************************************************
  * Create or retrieve a folder within a main folder based on the folder name.
@@ -29,18 +21,41 @@ function testFolder() {
  * console.log(`Folder Link: ${result.link}`);
  */
 
-function folderToSetData(mainFolderID, folderName) {
+function folderToSetData_(mainFolderID,folderName){
+  let foldersInMainFolder = listRootFolders_(mainFolderID)
+    let folderData = foldersInMainFolder.items.find(folder => (folder.name.toLowerCase() === folderName.toLowerCase() && folder.mimeType === 'application/vnd.google-apps.folder') )
 
-    let foldersInMainFolder = listRootFolders_(mainFolderID)
-    let folderData = foldersInMainFolder.items.find(folder =>
-        (folder.name.toLowerCase() === folderName && folder.mimeType === 'application/vnd.google-apps.folder'))
+  if (folderData)return {id: folderData.id, link: folderData.webViewLink}
+  else {
+    let mainFolder = DriveApp.getFolderById(mainFolderID);
+    let newFolder = mainFolder.createFolder(folderName)
+    return {id: newFolder.getId(), link: newFolder.getUrl()}
+  }  
+}
 
-    if (folderData) return { id: folderData.id, link: folderData.webViewLink }
-    else {
-        let mainFolder = DriveApp.getFolderById(mainFolderID);
-        let newFolder = mainFolder.createFolder(folderName)
-        return { id: newFolder.getId(), link: newFolder.getUrl() }
-    }
+
+
+function setUserFolder_(mainFolderID, userFolderName){
+if(!userFolderName) return;
+
+let currentDate = new Date()
+let month = (currentDate.getMonth() + 1).toString();
+let year = currentDate.getFullYear().toString()
+
+let yearFolder = folderToSetData_(mainFolderID,year)
+let monthFolder = folderToSetData_(yearFolder.id, MONTH_DESCRIPTION[month])
+let userFolder = folderToSetData_(monthFolder.id, userFolderName)
+
+}
+
+function testFolder(){
+  // let folder = setFolderToSetData_("1LTDyHFS4G-NXAauD1WwIUIfvD0yvI8CU", "2023")
+  // console.log(folder)
+
+  // let = folderData = folderToSetData("1LTDyHFS4G-NXAauD1WwIUIfvD0yvI8CU", "2023")
+  // console.log(folderData)
+
+  setUserFolder_("1LTDyHFS4G-NXAauD1WwIUIfvD0yvI8CU")
 }
 
 
