d1940d24663abeba723ec9ce686fcb64f0210dd0
Dzabol <sebastian.jablecki@gmail.com>
2024-01-27 21:33:05 +0100

    Added function to set folders

--- src/MainVariables/variables.js
+++ src/MainVariables/variables.js
@@ -1,2 +1,17 @@
 SOURCE_FOLDER_ID = "1rPPeQBJA8uv9aAJLAWjZ7L6COa8Jqpd9"
 DESTINATION_FOLDER_ID = "1LTDyHFS4G-NXAauD1WwIUIfvD0yvI8CU"
+
+const MONTH_DESCRIPTION = {
+    1: "1. Styczeń",
+    2: "2. Luty",
+    3: "3. Marzec",
+    4: "4. Kwiecień",
+    5: "5. Maj",
+    6: "6. Czerwiec",
+    7: "7. Lipiec",
+    8: "8. Sierpień",
+    9: "9. Wrzesień",
+    10: "10. Październik",
+    11: "11. Listopad",
+    12: "12. Grudzień",
+};
